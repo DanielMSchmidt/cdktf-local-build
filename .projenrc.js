234dd5ff1392b6b65ec470ef5d00813722f02725
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-12-30 20:58:40 +0100

    bootstrapped

---
+++ .projenrc.js
@@ -0,0 +1,18 @@
+const { CDKTFConstruct } = require('@dschmidt/cdktf-construct-base');
+const project = new CDKTFConstruct({
+  author: 'Daniel Schmidt',
+  authorAddress: 'danielmschmidt92@gmail.com',
+  defaultReleaseBranch: 'main',
+  name: 'cdktf-local-build',
+  repositoryUrl: 'https://github.com/DanielMSchmidt/cdktf-local-build.git',
+  description:
+    'A construct that encapsulates different building methods, e.g. for Node, Rust, Docker.',
+
+  devDeps: ['@dschmidt/cdktf-construct-base@0.0.1'],
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // release: undefined,      /* Add release management to this project. */
+});
+project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="classes"></a>
+
+### Hello <a name="cdktf-local-build.Hello" id="cdktflocalbuildhello"></a>
+
+#### Initializers <a name="cdktf-local-build.Hello.Initializer" id="cdktflocalbuildhelloinitializer"></a>
+
+```typescript
+import { Hello } from 'cdktf-local-build'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| [`sayHello`](#cdktflocalbuildhellosayhello) | *No description.* |
+
+---
+
+##### `sayHello` <a name="cdktf-local-build.Hello.sayHello" id="cdktflocalbuildhellosayhello"></a>
+
+```typescript
+public sayHello()
+```
+
+
+
+
+
