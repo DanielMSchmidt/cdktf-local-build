a388a16d336a866522db5ecd3f20c212f45fdbff
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-02-15 21:21:34 +0100

    Revert "chore: use docker based build pipeline"
    This reverts commit 5b4f5b21839e49c4225267efc358fc038beb61d5.

--- .projenrc.js
+++ .projenrc.js
@@ -14,17 +14,9 @@ const project = new CDKTFConstruct({
   bundledDeps: ["toml"],
   peerDeps: ["cdktf-local-exec@>=0.0.20", "@cdktf/provider-null@>=0.5.0"],
   gitignore: ["test/cargo/testproject/target/"],
-  workflowContainerImage: "danielmschmidt/cdktf-local-build:latest",
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
   // release: undefined,      /* Add release management to this project. */
 });
-
-project.addTask("buildDockerImage", {
-  description:
-    "Build the Docker image for this projects build image. Run manually for now.",
-  exec: "docker build -t danielmschmidt/cdktf-local-build:latest . && docker push danielmschmidt/cdktf-local-build:latest",
-});
-
 project.synth();
